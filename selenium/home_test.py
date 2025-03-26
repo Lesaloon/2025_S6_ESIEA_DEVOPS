b23e6d4d3effbfdc26dc5c44c8c040c45a9784c3
Nistro-dev <micmael86@gmail.com>
2025-03-26 17:19:18 +0100

    Update Selenium test driver configuration for headless execution and adjust server URL

--- selenium/home_test.py
+++ selenium/home_test.py
@@ -3,13 +3,22 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from homepage import HomePage
 import time
 
 @pytest.fixture(scope="function")
 def driver():
-    driver = webdriver.Chrome()
-    driver.get("http://localhost:5173")
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.binary_location = "/usr/bin/google-chrome"
+
+    service = Service("/usr/bin/chromedriver")
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.get("http://localhost:4173")
     
     yield driver
     driver.close()
